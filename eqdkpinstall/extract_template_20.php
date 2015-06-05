aa10b6b5439478f7ed114e2f6b02e88a407c5f55
Daniel <admin@guilddrive.de>
2015-06-06 01:42:39 +0200

    Update extract_template_20.php

--- eqdkpinstall/extract_template_20.php
+++ eqdkpinstall/extract_template_20.php
@@ -1,13 +1,17 @@
 <?php
+//#####################################*
+//» Script by Daniel@guilddrive.de
+//» https://guilddrive.de
+//» admin@guilddrive.de
+//» 05-06-2015
+
 function extract_eqdkp_template($var){
   $zip = new ZipArchive;
   if ($zip->open('/var/www/vhosts/domain.de/eqdkp_template20.zip') === TRUE) {
     $zip->extractTo("/var/www/vhosts/domain.de/$var/");
     $zip->close();
   }else{
-    $header = "From: Error-Reporting <email@tld.de>\r\n";
-    $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
-    mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "extract eqdkp template",$header);
+    errormail("ERROR:EQDKP_INSTALLATION","extract template 20 $subdomain");
   }
 }
 ?>
