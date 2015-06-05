44ff3639baa61667b5381fca9b6bc36fea3692a1
Daniel <admin@guilddrive.de>
2015-06-06 01:52:43 +0200

    Create localconf.gd

---
+++ localconf.gd
@@ -0,0 +1,229 @@
+<?php
+if (!defined('EQDKP_INC')){
+	die('You cannot access this file directly.');
+}
+$localconf = array (
+  'account_activation' => '1',
+  'active_point_adj' => '0.00',
+  'auth_method' => 'db',
+  'calendar_addevent_mode' => 'raid',
+  'calendar_addraid_deadline' => '1',
+  'calendar_addraid_duration' => '120',
+  'calendar_email_statuschange' => '1',
+  'calendar_raid_classbreak' => '5',
+  'calendar_raid_guests' => '1',
+  'calendar_raid_nsfilter' => 
+  array (
+    0 => 'twinks',
+    1 => 'inactive',
+    2 => 'hidden',
+  ),
+  'calendar_raid_status' => 
+  array (
+    0 => 0,
+    1 => 1,
+    2 => 2,
+    3 => 3,
+    4 => 4,
+  ),
+  'calendar_repeat_crondays' => '40',
+  'class_color' => '1',
+  'color_items' => 
+  array (
+    0 => 34,
+    1 => 67,
+  ),
+  'cookie_euhint_show' => '1',
+  'cookie_name' => 'eqdkp_6aa47d',
+  'cookie_path' => '/',
+  'custom_logo' => 'logo.svg',
+  'date_startday' => 'monday',
+  'debug' => '0',
+  'default_alimit' => '100',
+  'default_date_long' => 'j. F Y',
+  'default_date_short' => 'd.m.y',
+  'default_date_time' => 'H:i',
+  'default_elimit' => '100',
+  'default_game' => 'dummy',
+  'default_ilimit' => '100',
+  'default_lang' => 'german',
+  'default_locale' => 'de_DE',
+  'default_nlimit' => '10',
+  'default_rlimit' => '100',
+  'default_style' => '1',
+  'default_style_overwrite' => '0',
+  'dkp_name' => 'DKP',
+  'embedly_key' => '9bda60ed633e403e866dbca5f4c8e56d',
+  'enable_captcha' => '1',
+  'enable_comments' => '1',
+  'enable_embedly' => '1',
+  'enable_gzip' => '0',
+  'enable_leaderboard' => '1',
+  'enable_points' => '1',
+  'enable_registration' => '1',
+  'eqdkp_layout' => 'normal',
+  'eqdkp_start' => '1431351617',
+  'failed_logins_inactivity' => '5',
+  'game_language' => 'german',
+  'game_version' => '0.1.0',
+  'guildtag' => 'MyEQdkpPlus2.0@Guilddrive.de',
+  'hide_inactive' => '0',
+  'inactive_period' => '0',
+  'inactive_point_adj' => '0.00',
+  'infotooltip_use' => '0',
+  'itemhistory_dia' => '1',
+  'mainmenu' => 
+  array (
+    0 => 
+    array (
+      'item' => 
+      array (
+        'hash' => 'd41d8cd98f00b204e9800998ecf8427e',
+        'hidden' => '0',
+      ),
+    ),
+    1 => 
+    array (
+      'item' => 
+      array (
+        'hash' => 'e2672c7758bc5f8bb38ddb4b60fa530c',
+        'hidden' => '0',
+      ),
+    ),
+    2 => 
+    array (
+      'item' => 
+      array (
+        'hash' => '92f04bcfb72b27949ee68f52a412acac',
+        'hidden' => '0',
+      ),
+      '_childs' => 
+      array (
+        0 => 
+        array (
+          'item' => 
+          array (
+            'hash' => '7809b1008f1d915120b3b549ca033e1f',
+            'hidden' => '0',
+          ),
+        ),
+      ),
+    ),
+    3 => 
+    array (
+      'item' => 
+      array (
+        'hash' => 'ca65b9cf176197c365f17035270cc9f1',
+        'hidden' => '0',
+      ),
+      '_childs' => 
+      array (
+        1 => 
+        array (
+          'item' => 
+          array (
+            'hash' => '0e6acee4fa4635f2c25acbf0bad6c445',
+            'hidden' => '0',
+          ),
+        ),
+        2 => 
+        array (
+          'item' => 
+          array (
+            'hash' => '53433bf03b32b055f789428e95454cec',
+            'hidden' => '0',
+          ),
+        ),
+        3 => 
+        array (
+          'item' => 
+          array (
+            'hash' => 'c1ec6e24e3276e17e3edcb08655d9181',
+            'hidden' => '0',
+          ),
+        ),
+        4 => 
+        array (
+          'item' => 
+          array (
+            'hash' => '65d93e089c21a737b601f81e70921b8b',
+            'hidden' => '0',
+          ),
+        ),
+      ),
+    ),
+    4 => 
+    array (
+      'item' => 
+      array (
+        'hash' => '8f9bfe9d1345237cb3b2b205864da075',
+        'hidden' => '0',
+      ),
+    ),
+    5 => 
+    array (
+      'item' => 
+      array (
+        'hash' => 'ebc90e9afa50f8383d4f93ce9944b8dd',
+        'hidden' => '0',
+      ),
+      '_childs' => 
+      array (
+        5 => 
+        array (
+          'item' => 
+          array (
+            'hash' => '276753faf0f1a394d24bea5fa54a4e6b',
+            'hidden' => '0',
+          ),
+        ),
+        6 => 
+        array (
+          'item' => 
+          array (
+            'hash' => 'cd5f542b7201c8d9b8f697f97a2dcc52',
+            'hidden' => '0',
+          ),
+        ),
+      ),
+    ),
+    6 => 
+    array (
+      'item' => 
+      array (
+        'hash' => '292299380781735bd110e74fe0ada4ac',
+        'hidden' => '0',
+      ),
+    ),
+    7 => 
+    array (
+      'item' => 
+      array (
+        'hash' => '2a91cf06beec2894ebd9266c884558c3',
+        'hidden' => '0',
+      ),
+    ),
+  ),
+  'pdc' => 
+  array (
+    'dttl' => '86400',
+    'mode' => 'file',
+    'prefix' => 'eqdkp20_',
+  ),
+  'pk_attendance90' => '1',
+  'pk_lastraid' => '1',
+  'pk_maintenance_mode' => '1',
+  'pk_newsloot_limit' => 'all',
+  'plus_version' => '2.0.2.0',
+  'round_activate' => '1',
+  'round_precision' => '2',
+  'seo_extension' => '1',
+  'server_path' => '/',
+  'session_cleanup' => '0',
+  'session_last_cleanup' => '0',
+  'session_length' => '3600',
+  'start_page' => 'news',
+  'thumbnail_defaultsize' => '500',
+  'timezone' => 'Europe/Paris',
+);
+?>
