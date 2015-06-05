19b83a9c65749c061b87db59a54c03917e5be101
Daniel <admin@guilddrive.de>
2015-06-05 17:45:18 +0200

    Update create_db_20.php

--- eqdkpinstall/create_db_20.php
+++ eqdkpinstall/create_db_20.php
@@ -2,25 +2,25 @@
 
 function import_eqdkp_db($var){
 
-  $mysqlDatabaseName ="$var";
-  $mysqlUserName ='dbuser';
-  $mysqlPassword ='password';
-  $mysqlHostName ='localhost';
-  $mysqlImportFilename ='/var/www/vhosts/domain.de/httpdocs/docroot/sqltmp/' . $var . '.sql';
-		
-  //=> import mysql-template and request ist
-  $command='mysql -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' < ' .$mysqlImportFilename;
-  $output=array();
-  exec($command,$output,$worked);
-  switch($worked){
-  case 0:
-  	break;
-  case 1:
-	$header = "From: Error-Reporting <email@tld.de>\r\n";
-	$header .= "Content-Type: text/plain; Charset=utf-8\r\n";
-	mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "import eqdkp db",$header);
-  	break;
-  }
+	$dbhost = 'localhost';
+	$dbuser = 'dbuser';
+	$dbname = "$var";
+	$dbpass = 'password';
+	$dbimportfilename ='/var/www/vhosts/domain.de/httpdocs/docroot/sqltmp/' . $var . '.sql';
+  
+	//=> import mysql-template and request it
+	$command='mysql -h' .$dbhost .' -u' .$dbuser .' -p' .$dbpass .' ' .$dbname .' < ' .$dbimportfilename;
+	$output=array();
+	exec($command,$output,$worked);
+	switch($worked){
+	case 0:
+		break;
+	case 1:
+		$header = "From: Error-Reporting <email@tld.de>\r\n";
+		$header .= "Content-Type: text/plain; Charset=utf-8\r\n";
+		mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "import eqdkp db",$header);
+	  	break;
+	}
 }
 
 ?> 
