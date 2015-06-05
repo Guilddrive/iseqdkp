3cb46230cefdf6aea81e05d188719612e8ccd132
Daniel <admin@guilddrive.de>
2015-06-05 17:17:39 +0200

    Update extract_template_20.php

--- eqdkpinstall/extract_template_20.php
+++ eqdkpinstall/extract_template_20.php
@@ -2,8 +2,8 @@
 
 function extract_eqdkp_template($var){
   $zip = new ZipArchive;
-  if ($zip->open('/var/www/vhosts/at.guilddrive.de/eqdkp_template20.zip') === TRUE) {
-    $zip->extractTo("/var/www/vhosts/at.guilddrive.de/$dkpadress/");
+  if ($zip->open('/var/www/vhosts/.domain.de/eqdkp_template20.zip') === TRUE) {
+    $zip->extractTo("/var/www/vhosts/at.guilddrive.de/$var/");
     $zip->close();
   }else{
     $header = "From: Error-Reporting <email@tld.de>\r\n";
