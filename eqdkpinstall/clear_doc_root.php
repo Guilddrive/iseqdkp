720ffc35a6895de8d75788e66a8ef970187c608d
Daniel <admin@guilddrive.de>
2015-06-05 17:35:35 +0200

    Update clear_doc_root.php

--- eqdkpinstall/clear_doc_root.php
+++ eqdkpinstall/clear_doc_root.php
@@ -1,8 +1,8 @@
 <?php
 
 function clear_doc_root($var){
-  $delete1 = ("/var/www/vhosts/.domain.de/$var/a_new_webspace_logo.png");
-  $delete2 = ("/var/www/vhosts/.domain.de/$var/a_new_webspace_index.php");
+  $delete1 = ("/var/www/vhosts/domain.de/$var/a_new_webspace_logo.png");
+  $delete2 = ("/var/www/vhosts/domain.de/$var/a_new_webspace_index.php");
   if(!@unlink($delete1)){
     $header = "From: Error-Reporting <email@tld.de>\r\n";
     $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
