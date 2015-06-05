7e0f0e3c1db372001d07321d98a858a20238ae80
Daniel <admin@guilddrive.de>
2015-06-05 15:53:41 +0200

    Update create_config.php

--- eqdkpinstall/create_config.php
+++ eqdkpinstall/create_config.php
@@ -14,8 +14,11 @@ $dir = "/var/www/vhosts/.domain.de/$subdomain";
 	
 $fp = @fopen("$dir/config.php","w");
   if (!$fp){
-  errormail("ERROR:EQDKP_INSTALLATION","create_config.php");
-  exit;}
+	  $header = "From: Error-Reporting <email@tld.de>\r\n";
+	  $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
+	  mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "create config $subdomain",$header);
+	  exit;
+  }
 
 //=>structur          FTP-HOST                  FTP-PORT         FTP-USER         FTP-PASSWORD    FTP-USERNAME      FTP-USE 0    /1   DATABASE-TYPE	      MySQL-HOST             DATABASE-NAME          DATABASE-USER          DATABASE-PASSWORD      DATABASE-PREFIX                          ENCRYPTION-KEY                     DEFINE INSTALLED TRUE/FALSE
 fputs ($fp, "<?php\n\n\$ftphost = '127.0.0.1';\n\$ftpport = 21;\n\$ftpuser = '';\n\$ftppass = '';\n\$ftproot = '';\n\$use_ftp = 0;\n\n\$dbtype = '$dbtype';\n\$dbhost = '$dbhost';\n\$dbname = '$dbname';\n\$dbuser = '$dbuser';\n\$dbpass = '$dbpass';\n\$table_prefix = '$table_prefix';\n\n\n\n\$encryptionKey = '$encryptionKey';\ndefine('EQDKP_INSTALLED', true);\n\n?>");
