c65f5d746c220222e79d5caa7ed1e50ab157c358
Daniel <admin@guilddrive.de>
2015-06-06 01:43:29 +0200

    Update hash_and_rename.php

--- eqdkpinstall/hash_and_rename.php
+++ eqdkpinstall/hash_and_rename.php
@@ -1,11 +1,15 @@
 <?php
+//#####################################*
+//» Script by Daniel@guilddrive.de
+//» https://guilddrive.de
+//» admin@guilddrive.de
+//» 05-06-2015
+
 function hash_and_rename($var){
   $hash = md5($var . "_" . $var); //=> md5(mysql_prefix+databasename)
   $rename = rename("/var/www/vhosts/domain.de/$var.domain.de/data/dkphash", "/var/www/vhosts/domain.de/$var.domain.de/data/$hash");
     if($rename == false){
-      $header = "From: Error-Reporting <email@tld.de>\r\n";
-      $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
-      mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "hash and rename $var",$header);
+      errormail("ERROR:EQDKP_INSTALLATION","hash and rename $var");
     }
 }
 ?>
