67c25b29338ed032536fa2a3e41e8f17007f0411
Daniel <admin@guilddrive.de>
2015-06-05 17:51:45 +0200

    Update create_config.php

--- eqdkpinstall/create_config.php
+++ eqdkpinstall/create_config.php
@@ -1,5 +1,4 @@
 <?php
-
 function create_eqdkp_config($var, $subdomain, $key){
 
 $dbtype = 'mysqli';
@@ -7,7 +6,7 @@ $dbhost = 'localhost';
 $dbuser = 'dbuser';
 $dbname = "$var";
 $dbpass = 'password'; //=>set mysql password
-$table_prefix = "" . $dkpname . "_";
+$table_prefix = "" . $var . "_";
 	
 $encryptionKey = md5(md5(md5($key)));
 $dir = "/var/www/vhosts/domain.de/$subdomain";
@@ -24,5 +23,4 @@ $fp = @fopen("$dir/config.php","w");
 fputs ($fp, "<?php\n\n\$ftphost = '127.0.0.1';\n\$ftpport = 21;\n\$ftpuser = '';\n\$ftppass = '';\n\$ftproot = '';\n\$use_ftp = 0;\n\n\$dbtype = '$dbtype';\n\$dbhost = '$dbhost';\n\$dbname = '$dbname';\n\$dbuser = '$dbuser';\n\$dbpass = '$dbpass';\n\$table_prefix = '$table_prefix';\n\n\n\n\$encryptionKey = '$encryptionKey';\ndefine('EQDKP_INSTALLED', true);\n\n?>");
 fclose($fp);
 }
-
 ?>
