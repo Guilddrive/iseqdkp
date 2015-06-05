35a51fdd53187b8da7490fd6e9d5abb52eb32dc5
Daniel <admin@guilddrive.de>
2015-06-06 01:38:30 +0200

    Update copy_and_rename_20.php

--- eqdkpinstall/copy_and_rename_20.php
+++ eqdkpinstall/copy_and_rename_20.php
@@ -1,12 +1,16 @@
 <?php
+//#####################################*
+//» Script by Daniel@guilddrive.de
+//» https://www.guilddrive.de
+//» admin@guilddrive.de
+//» 05-06-2015
+
 function copy_and_rename($var){
   $file = "/var/www/vhosts/domain.de/hosttemplate20.sql"; //=> mysql-template
   $newfile = "/var/www/vhosts/domain.de/httpdocs/docroot/sqltmp/" . $var . ".sql"; //=> filename = prefix
 			
   if (!copy($file, $newfile)) {
-    $header = "From: Error-Reporting <error@tld.de>\r\n";
-    $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
-    mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "copy and rename $var",$header);
+  	errormail("ERROR:EQDKP_INSTALLATION","copy and rename $sqlname");
   }
 			
   //=> adjust prefix
