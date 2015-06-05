486e04434da1e19600f44fc3a10df24d6a18bbc3
Daniel <admin@guilddrive.de>
2015-06-05 17:12:23 +0200

    Update set_permissions.php

--- eqdkpinstall/set_permissions.php
+++ eqdkpinstall/set_permissions.php
@@ -1,10 +1,9 @@
 <?php
 //=> set permissions 0644
-function set_permission_0644($file_or_dir)
-{
-$datei = "$file_or_dir";
-$rechte = 0644;
-  if(!@chmod($datei, $rechte) == true) {
+function set_permission_0644($var){
+$file = "$var";
+$perm = 0644;
+  if(!@chmod($file, $perm) == true) {
     $header = "From: Error-Reporting <email@tld.de>\r\n";
     $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
     mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "set permissions to 0644",$header);
@@ -12,11 +11,10 @@ $rechte = 0644;
 }
 	
 //=> set permissions 0777
-function set_permission_0777($file_or_dir)
-{
-$datei = "$file_or_dir";
-$rechte = 0777;
-  if(!@chmod($datei, $rechte) == true) {
+function set_permission_0777($var){
+$file = "$var";
+$perm = 0777;
+  if(!@chmod($file, $perm) == true) {
     $header = "From: Error-Reporting <email@tld.de>\r\n";
     $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
     mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "set permissions to 0777",$header);
