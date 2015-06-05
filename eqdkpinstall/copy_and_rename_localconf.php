89583d3ae8dd6037fb3363f721d8fbaf6e661d69
Daniel <admin@guilddrive.de>
2015-06-05 17:46:58 +0200

    Update copy_and_rename_localconf.php

--- eqdkpinstall/copy_and_rename_localconf.php
+++ eqdkpinstall/copy_and_rename_localconf.php
@@ -2,8 +2,8 @@
 //=> important notice: before hashing md5(mysql_prefix+databasename) the folder musst be called dkphash!
 	
 function copy_and_rename_localconf($var){
-  $file = "/var/www/vhosts/.domain.de/localconf.gd"; //=> file-template
-  $newfile = "/var/www/vhosts/.domain.de/$var.domain.de/data/dkphash/eqdkp/config/localconf.php";
+  $file = "/var/www/vhosts/domain.de/localconf.gd"; //=> file-template
+  $newfile = "/var/www/vhosts/domain.de/$var.domain.de/data/dkphash/eqdkp/config/localconf.php";
 			
   if (!copy($file, $newfile)) {
     $header = "From: Error-Reporting <error@tld.de>\r\n";
@@ -14,7 +14,7 @@ function copy_and_rename_localconf($var){
   //=> adjust copied file
   $search = eqdkp20_;
   $replace = "" . $var . "_";
-  $file = "/var/www/vhosts/at.guilddrive.de/$dkpname.at.guilddrive.de/data/dkphash/eqdkp/config/localconf.php";
+  $file = "/var/www/vhosts/domain.de/$var.domain.de/data/dkphash/eqdkp/config/localconf.php";
   $content = file_get_contents($file);
   $content = str_replace($search, $replace, $content);
   $fh = fopen($file, "w");
