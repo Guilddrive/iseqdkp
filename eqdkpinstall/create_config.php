d0832e893bd9e9c3f9ca84878d73c4ed424ed448
Daniel <admin@guilddrive.de>
2015-06-05 17:38:41 +0200

    Update create_config.php

--- eqdkpinstall/create_config.php
+++ eqdkpinstall/create_config.php
@@ -20,7 +20,7 @@ $fp = @fopen("$dir/config.php","w");
 	  exit;
   }
 
-//=>structur          FTP-HOST                  FTP-PORT         FTP-USER         FTP-PASSWORD    FTP-USERNAME      FTP-USE 0    /1   DATABASE-TYPE	      MySQL-HOST             DATABASE-NAME          DATABASE-USER          DATABASE-PASSWORD      DATABASE-PREFIX                          ENCRYPTION-KEY                     DEFINE INSTALLED TRUE/FALSE
+//=>structur          ftp-HOST                  ftp-PORT         ftp-USER         ftp-PASSWORD    ftp-USERNAME      ftp-USE 0    /1   database-TYPE	      mysql-HOST             database-NAME          database-USER          database-PASSWORD      database-PREFIX                          encryption-KEY                     define installe TRUE/FALSE
 fputs ($fp, "<?php\n\n\$ftphost = '127.0.0.1';\n\$ftpport = 21;\n\$ftpuser = '';\n\$ftppass = '';\n\$ftproot = '';\n\$use_ftp = 0;\n\n\$dbtype = '$dbtype';\n\$dbhost = '$dbhost';\n\$dbname = '$dbname';\n\$dbuser = '$dbuser';\n\$dbpass = '$dbpass';\n\$table_prefix = '$table_prefix';\n\n\n\n\$encryptionKey = '$encryptionKey';\ndefine('EQDKP_INSTALLED', true);\n\n?>");
 fclose($fp);
 }
