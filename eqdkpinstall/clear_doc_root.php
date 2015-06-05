4e558c255e2012a74611c5620c44e5821ac26c72
Daniel <admin@guilddrive.de>
2015-06-05 15:44:26 +0200

    Update clear_doc_root.php

--- eqdkpinstall/clear_doc_root.php
+++ eqdkpinstall/clear_doc_root.php
@@ -3,8 +3,16 @@
 function clear_doc_root($dkpadress){
   $delete1 = ("/var/www/vhosts/at.guilddrive.de/$dkpadress/rock.css");
   $delete2 = ("/var/www/vhosts/at.guilddrive.de/$dkpadress/index.html");
-  if((!@unlink($delete1))||(!@unlink($delete2)))
-    errormail("ERROR:EQDKP_INSTALLATION","clear_doc_root.php");
+  if(!@unlink($delete1)){
+    $header = "From: Error-Reporting <email@tld.de>\r\n";
+    $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
+    mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "clear doc root var1",$header);
+  }
+  if(!@unlink($delete2)){
+    $header = "From: Error-Reporting <email@tld.de>\r\n";
+    $header .= "Content-Type: text/plain; Charset=utf-8\r\n";
+    mail("admin@tld.de", "ERROR:EQDKP_INSTALLATION", "clear doc root var2",$header);
+  }
 }
 
 ?>
