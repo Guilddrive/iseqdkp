0dcb69f9409adf774b2675b06c3b034d9db07092
Daniel <admin@guilddrive.de>
2015-06-07 20:31:57 +0200

    Update extract_template_20.php

--- eqdkpinstall/extract_template_20.php
+++ eqdkpinstall/extract_template_20.php
@@ -8,10 +8,11 @@
 function extract_eqdkp_template($path, $subdomain){
   $zip = new ZipArchive;
   if ($zip->open($path . "eqdkp_template20.zip") === TRUE) {
-    $zip->extractTo($path$subdomain . "/");
+    $zip->extractTo($path . "" . $subdomain . "/");
     $zip->close();
   }else{
-    errormail("ERROR:EQDKP_INSTALLATION","extract template 20 $path$subdomain");
+    errormail("ERROR:EQDKP_INSTALLATION","extract template 20 path: $path subdomain: $subdomain");
   }
 }
 ?>
+
