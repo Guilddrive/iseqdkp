ab75dd951c7067c4d10651d7794742791296ec55
Daniel <admin@guilddrive.de>
2015-06-07 13:52:46 +0200

    Update set_permissions.php

--- eqdkpinstall/set_permissions.php
+++ eqdkpinstall/set_permissions.php
@@ -5,19 +5,9 @@
 //» admin@guilddrive.de
 //» 05-06-2015
 
-function set_permission_0644($var){
-	$file = "$var";
-	$perm = 0644;
+function set_permission($file, $perm){
 	if(!@chmod($file, $perm) == true) {
-		errormail("ERROR:EQDKP_INSTALLATION","set permissions 0644 $var");
-	}
-}
-	
-function set_permission_0777($var){
-	$file = "$var";
-	$perm = 0777;
-	if(!@chmod($file, $perm) == true) {
-		errormail("ERROR:EQDKP_INSTALLATION","set permissions 0777 $var");
+		errormail("ERROR:EQDKP_INSTALLATION","set permissions $perm $file");
 	}
 }
 ?>
