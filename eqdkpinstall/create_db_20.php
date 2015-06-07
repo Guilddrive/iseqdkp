c793d4d5f126db42bf3a7c20a388548cb790bba7
Daniel <admin@guilddrive.de>
2015-06-07 20:33:21 +0200

    Update create_db_20.php

--- eqdkpinstall/create_db_20.php
+++ eqdkpinstall/create_db_20.php
@@ -1,21 +1,21 @@
 <?php
 //#####################################*
-//» Script by Daniel@guilddrive.de
-//» https://guilddrive.de
+//» Script by Daniel@www.guilddrive.de
+//» http://www.guilddrive.de
 //» admin@guilddrive.de
 //» 05-06-2015
 
 function import_eqdkp_db($path, $dbname, $dbuser, $dbpass){
 	$dbhost = 'localhost';
-	$mysqlImportFilename = $path . "httpdocs/eqdkpinstall/sqltmp/" . $dbname . ".sql";
-	$command='mysql -h' . $dbhost . ' -u' . $dbuser . ' -p' . $dbpass . ' ' . $dbname . ' < ' .$mysqlImportFilename;
+	$mysqlImportFilename = $path. "httpdocs/eqdkpinstall/sqltmp/" . $dbname . ".sql";
+	$command="mysql -h" . $dbhost . " -u" . $dbuser . " -p" . $dbpass . " " . $dbname . " < " . $mysqlImportFilename;
 	$output=array();
 	exec($command,$output,$worked);
 		switch($worked){
 		case 0:
 			break;
 		case 1:
-			errormail("ERROR:EQDKP_INSTALLATION","create db 20 $path DB: $dbname DB-User: $dbuser DB-Passwd: $dbpass");
+			errormail("ERROR:EQDKP_INSTALLATION","create db 20 $mysqlImportFilename");
 			break;
 		}
 }
